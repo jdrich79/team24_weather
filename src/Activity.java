5c28b230e17a157e64f7b399c9b4ab40ccc91604
croot22 <croot22@seas.upenn.edu>
2020-04-02 17:58:58 -0600

    changing variables for activity class

--- src/Activity.java
+++ src/Activity.java
@@ -6,149 +6,45 @@
  *
  */
 public class Activity {
-
-    private Double bestqpf;
-    private Double bestqpfSnow;
-    private Integer bestTemperatureMax;
-    private Integer bestTemperatureMin;
-    //day and night
-    private Integer bestPrecipChanceD;
-    private Integer bestPrecipChanceN;
-    private Integer bestCloudCoverD;
-    private Integer bestCloudCoverN;
-    private String bestPrecipTypeD;
-    private String bestPrecipTypeN;
-    private Double bestqpfD;
-    private Double bestqpfN;
-    private Double bestqpfSnowD;
-    private Double bestqpfSnowN;
-    private String bestSnowRangeD;
-    private String bestSnowRangeN;
-    private Integer bestTemperatureHeatIndexD;
-    private Integer bestTemperatureHeatIndexN;
-    private Integer bestTemperatureWindChillD;
-    private Integer bestTemperatureWindChillN;
-    private String bestWindPhraseD;
-    private String bestWindPhraseN;
+	private int bestIndex;
+	private double bestHighTemp;
+	private double bestLowTemp;
+	private String temperatureUnit;
+	private String bestWindSpeed;
+	private String windDirection;
+	private String icon;
+	private String shortForecast;
+	private String longForecast;
+	private double bestProbabilityOfPrecip;
+	private double bestAmountOfPrecip;
 	
-    
-
-	public Double getBestqpf() {
-		return bestqpf;
-	}
-
-	public Double getBestqpfSnow() {
-		return bestqpfSnow;
-	}
-
-	public Integer getBestTemperatureMax() {
-		return bestTemperatureMax;
-	}
-
-	public Integer getBestTemperatureMin() {
-		return bestTemperatureMin;
-	}
-
-	public Integer getBestPrecipChanceD() {
-		return bestPrecipChanceD;
-	}
-
-	public Integer getBestPrecipChanceN() {
-		return bestPrecipChanceN;
-	}
-
-	public Integer getBestCloudCoverD() {
-		return bestCloudCoverD;
-	}
-
-	public Integer getBestCloudCoverN() {
-		return bestCloudCoverN;
-	}
-
-	public String getBestPrecipTypeD() {
-		return bestPrecipTypeD;
-	}
-
-	public String getBestPrecipTypeN() {
-		return bestPrecipTypeN;
-	}
-
-	public Double getBestqpfD() {
-		return bestqpfD;
-	}
-
-	public Double getBestqpfN() {
-		return bestqpfN;
-	}
-
-	public Double getBestqpfSnowD() {
-		return bestqpfSnowD;
-	}
-
-	public Double getBestqpfSnowN() {
-		return bestqpfSnowN;
-	}
-
-	public String getBestSnowRangeD() {
-		return bestSnowRangeD;
-	}
-
-	public String getBestSnowRangeN() {
-		return bestSnowRangeN;
-	}
-
-	public Integer getBestTemperatureHeatIndexD() {
-		return bestTemperatureHeatIndexD;
-	}
-
-	public Integer getBestTemperatureHeatIndexN() {
-		return bestTemperatureHeatIndexN;
-	}
-
-	public Integer getBestTemperatureWindChillD() {
-		return bestTemperatureWindChillD;
-	}
-
-	public Integer getBestTemperatureWindChillN() {
-		return bestTemperatureWindChillN;
-	}
-
-	public String getBestWindPhraseD() {
-		return bestWindPhraseD;
-	}
-
-	public String getBestWindPhraseN() {
-		return bestWindPhraseN;
-	}
-
 	public void sailing() {
-		this.bestTemp = 0;
-		this.bestPrecip = 0;
-		this.bestWindSpeed= 0;
+		this.bestHighTemp = 75;
+		this.bestLowTemp = 65;
+		this.temperatureUnit = ;
+		this.bestWindSpeed;
+		this.windDirection;
+		this.icon;
+		this.shortForecast;
+		this.longForecast;
+		this.bestProbabilityOfPrecip;
+		this.bestAmountOfPrecip;
 	}
 	
 	public void hiking() {
-		this.bestTemp = 0;
-		this.bestPrecip = 0;
-		this.bestWindSpeed= 0;
+
 	}
 	
 	public void climbing() {
-		this.bestTemp = 0;
-		this.bestPrecip = 0;
-		this.bestWindSpeed= 0;
+
 	}
 	
 	public void skiing() {
-		this.bestTemp = 0;
-		this.bestPrecip = 0;
-		this.bestWindSpeed= 0;
+
 	}
 	
 	public void biking() {
-		this.bestTemp = 0;
-		this.bestPrecip = 0;
-		this.bestWindSpeed= 0;
+
 	}
 	
 }
