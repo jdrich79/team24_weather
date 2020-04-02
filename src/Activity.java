4353859e0330fbb6cac3bef0380cef716642c244
croot22 <croot22@seas.upenn.edu>
2020-04-02 17:49:11 -0600

    adding weather variables to activity class

--- src/Activity.java
+++ src/Activity.java
@@ -6,20 +6,119 @@
  *
  */
 public class Activity {
-	private int bestTemp;
-	private int bestPrecip;
-	private int bestWindSpeed;
+
+    private Double bestqpf;
+    private Double bestqpfSnow;
+    private Integer bestTemperatureMax;
+    private Integer bestTemperatureMin;
+    //day and night
+    private Integer bestPrecipChanceD;
+    private Integer bestPrecipChanceN;
+    private Integer bestCloudCoverD;
+    private Integer bestCloudCoverN;
+    private String bestPrecipTypeD;
+    private String bestPrecipTypeN;
+    private Double bestqpfD;
+    private Double bestqpfN;
+    private Double bestqpfSnowD;
+    private Double bestqpfSnowN;
+    private String bestSnowRangeD;
+    private String bestSnowRangeN;
+    private Integer bestTemperatureHeatIndexD;
+    private Integer bestTemperatureHeatIndexN;
+    private Integer bestTemperatureWindChillD;
+    private Integer bestTemperatureWindChillN;
+    private String bestWindPhraseD;
+    private String bestWindPhraseN;
 	
-	public int getBestTemp() {
-		return bestTemp;
+    
+
+	public Double getBestqpf() {
+		return bestqpf;
 	}
 
-	public int getBestPrecip() {
-		return bestPrecip;
+	public Double getBestqpfSnow() {
+		return bestqpfSnow;
 	}
 
-	public int getBestWindSpeed() {
-		return bestWindSpeed;
+	public Integer getBestTemperatureMax() {
+		return bestTemperatureMax;
+	}
+
+	public Integer getBestTemperatureMin() {
+		return bestTemperatureMin;
+	}
+
+	public Integer getBestPrecipChanceD() {
+		return bestPrecipChanceD;
+	}
+
+	public Integer getBestPrecipChanceN() {
+		return bestPrecipChanceN;
+	}
+
+	public Integer getBestCloudCoverD() {
+		return bestCloudCoverD;
+	}
+
+	public Integer getBestCloudCoverN() {
+		return bestCloudCoverN;
+	}
+
+	public String getBestPrecipTypeD() {
+		return bestPrecipTypeD;
+	}
+
+	public String getBestPrecipTypeN() {
+		return bestPrecipTypeN;
+	}
+
+	public Double getBestqpfD() {
+		return bestqpfD;
+	}
+
+	public Double getBestqpfN() {
+		return bestqpfN;
+	}
+
+	public Double getBestqpfSnowD() {
+		return bestqpfSnowD;
+	}
+
+	public Double getBestqpfSnowN() {
+		return bestqpfSnowN;
+	}
+
+	public String getBestSnowRangeD() {
+		return bestSnowRangeD;
+	}
+
+	public String getBestSnowRangeN() {
+		return bestSnowRangeN;
+	}
+
+	public Integer getBestTemperatureHeatIndexD() {
+		return bestTemperatureHeatIndexD;
+	}
+
+	public Integer getBestTemperatureHeatIndexN() {
+		return bestTemperatureHeatIndexN;
+	}
+
+	public Integer getBestTemperatureWindChillD() {
+		return bestTemperatureWindChillD;
+	}
+
+	public Integer getBestTemperatureWindChillN() {
+		return bestTemperatureWindChillN;
+	}
+
+	public String getBestWindPhraseD() {
+		return bestWindPhraseD;
+	}
+
+	public String getBestWindPhraseN() {
+		return bestWindPhraseN;
 	}
 
 	public void sailing() {
