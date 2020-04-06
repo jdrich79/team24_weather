053f3b9a9c50ac3207e99f4ce785e6e66670c15f
croot22 <croot22@seas.upenn.edu>
2020-04-06 07:16:30 -0600

    added in ranking method based on simple average

--- src/Activity.java
+++ src/Activity.java
@@ -6,86 +6,138 @@
  *
  */
 public class Activity {
-    private Integer bestTemperatureMax;
-    private Integer bestTemperatureMin;
-    
-    // day part
-    private Integer bestPrecipChance;
-    private Integer bestCloudCover;
-    private Double bestQpf;
-    private Double bestQpfSnow;
-    private Integer bestTemperatureHeatIndex;
-    private Integer bestTemperatureWindChill;
-    private String bestWindPhrase;
-	
-	public void sailing() {
-	   this.bestTemperatureMax = 75;
-	   this.bestTemperatureMin = 65;
-	    
-	    // day part
-	    this.bestPrecipChance = 0;
-	    this.bestCloudCover = 3;
-	    this.bestQpf = 0.0;
-	    this.bestQpfSnow = 0.0;
-	    this.bestTemperatureHeatIndex = 78;
-	    this.bestTemperatureWindChill = 77;
-	    this.bestWindPhrase = "";
+	private Integer bestTemperatureMax;
+	private Integer bestTemperatureMin;
+
+	// day part
+	private Integer bestPrecipChance;
+	private Integer bestCloudCover;
+	private Double bestQpf;
+	private Double bestQpfSnow;
+	private Integer bestTemperatureHeatIndex;
+	private Integer bestTemperatureWindChill;
+	private String bestWindPhrase;
+
+	public Integer getBestTemperatureMax() {
+		return bestTemperatureMax;
 	}
-	
-	public void hiking() {
-		   this.bestTemperatureMax = 68;
-		   this.bestTemperatureMin = 60;
-		    
-		    // day part
-		    this.bestPrecipChance = 0;
-		    this.bestCloudCover = 4;
-		    this.bestQpf = 0.0;
-		    this.bestQpfSnow = 0.0;
-		    this.bestTemperatureHeatIndex = 70;
-		    this.bestTemperatureWindChill = 70;
-		    this.bestWindPhrase = "";
+
+	public Integer getBestTemperatureMin() {
+		return bestTemperatureMin;
 	}
-	
-	public void climbing() {
-		   this.bestTemperatureMax = 72;
-		   this.bestTemperatureMin = 65;
-		    
-		    // day part
-		    this.bestPrecipChance = 0;
-		    this.bestCloudCover = 1;
-		    this.bestQpf = 0.0;
-		    this.bestQpfSnow = 0.0;
-		    this.bestTemperatureHeatIndex = 74;
-		    this.bestTemperatureWindChill = 73;
-		    this.bestWindPhrase = "";
+
+	public Integer getBestPrecipChance() {
+		return bestPrecipChance;
 	}
-	
-	public void skiing() {
-		   this.bestTemperatureMax = 25;
-		   this.bestTemperatureMin = 20;
-		    
-		    // day part
-		    this.bestPrecipChance = 0;
-		    this.bestCloudCover = 3;
-		    this.bestQpf = 0.0;
-		    this.bestQpfSnow = 0.25;
-		    this.bestTemperatureHeatIndex = 25;
-		    this.bestTemperatureWindChill = 25;
-		    this.bestWindPhrase = "";
+
+	public Integer getBestCloudCover() {
+		return bestCloudCover;
 	}
-	
-	public void biking() {
-		   this.bestTemperatureMax = 72;
-		   this.bestTemperatureMin = 65;
-		    
-		    // day part
-		    this.bestPrecipChance = 0;
-		    this.bestCloudCover = 3;
-		    this.bestQpf = 0.0;
-		    this.bestQpfSnow = 0.0;
-		    this.bestTemperatureHeatIndex = 74;
-		    this.bestTemperatureWindChill = 72;
-		    this.bestWindPhrase = "";
+
+	public Double getBestQpf() {
+		return bestQpf;
 	}
-	
+
+	public Double getBestQpfSnow() {
+		return bestQpfSnow;
+	}
+
+	public Integer getBestTemperatureHeatIndex() {
+		return bestTemperatureHeatIndex;
+	}
+
+	public Integer getBestTemperatureWindChill() {
+		return bestTemperatureWindChill;
+	}
+
+	public String getBestWindPhrase() {
+		return bestWindPhrase;
+	}
+
+	public Activity() {
+
+		this.bestTemperatureMax = 0;
+		this.bestTemperatureMin = 0;
+
+		// day part
+		this.bestPrecipChance = 0;
+		this.bestCloudCover = 0;
+		this.bestQpf = 0.0;
+		this.bestQpfSnow = 0.0;
+		this.bestTemperatureHeatIndex = 0;
+		this.bestTemperatureWindChill = 0;
+		this.bestWindPhrase = "";
+	}
+
+	/**
+	 * selectActivity allows you to set the parameters for the chosen activity. This method does not return anything/ 
+	 * @param activity
+	 */
+	public void selectActivity(String activity) {
+		if (activity == "sailing") {
+			this.bestTemperatureMax = 75;
+			this.bestTemperatureMin = 65;
+
+			// day part
+			this.bestPrecipChance = 0;
+			this.bestCloudCover = 3;
+			this.bestQpf = 0.0;
+			this.bestQpfSnow = 0.0;
+			this.bestTemperatureHeatIndex = 78;
+			this.bestTemperatureWindChill = 77;
+			this.bestWindPhrase = "";
+		}
+		else if (activity == "hiking") {
+			this.bestTemperatureMax = 68;
+			this.bestTemperatureMin = 60;
+
+			// day part
+			this.bestPrecipChance = 0;
+			this.bestCloudCover = 4;
+			this.bestQpf = 0.0;
+			this.bestQpfSnow = 0.0;
+			this.bestTemperatureHeatIndex = 70;
+			this.bestTemperatureWindChill = 70;
+			this.bestWindPhrase = "";
+		}
+		else if (activity == "climbing") {
+			this.bestTemperatureMax = 72;
+			this.bestTemperatureMin = 65;
+
+			// day part
+			this.bestPrecipChance = 0;
+			this.bestCloudCover = 1;
+			this.bestQpf = 0.0;
+			this.bestQpfSnow = 0.0;
+			this.bestTemperatureHeatIndex = 74;
+			this.bestTemperatureWindChill = 73;
+			this.bestWindPhrase = "";
+		}
+		else if (activity == "skiing") {
+			this.bestTemperatureMax = 25;
+			this.bestTemperatureMin = 20;
+
+			// day part
+			this.bestPrecipChance = 0;
+			this.bestCloudCover = 3;
+			this.bestQpf = 0.0;
+			this.bestQpfSnow = 0.25;
+			this.bestTemperatureHeatIndex = 25;
+			this.bestTemperatureWindChill = 25;
+			this.bestWindPhrase = "";
+		}
+		else if (activity == "biking") {
+			this.bestTemperatureMax = 72;
+			this.bestTemperatureMin = 65;
+
+			// day part
+			this.bestPrecipChance = 0;
+			this.bestCloudCover = 3;
+			this.bestQpf = 0.0;
+			this.bestQpfSnow = 0.0;
+			this.bestTemperatureHeatIndex = 74;
+			this.bestTemperatureWindChill = 72;
+			this.bestWindPhrase = "";
+		}
+	}
 }
--- src/RankForecast.java
+++ src/RankForecast.java
@@ -1,4 +1,4 @@
-  
+
 import java.util.ArrayList;
 /**
  * This class is responsible for knowing forecasts, knowing activities, and ranking days/times by chosen activities
@@ -7,16 +7,55 @@ import java.util.ArrayList;
  *
  */
 public class RankForecast {
-	private ArrayList<String> rankedList = new ArrayList<String>();
+	private ArrayList<FiveDayForecast> weatherList = new ArrayList<FiveDayForecast>();
 	private Activity activity = new Activity();
-	
-	public ArrayList<String> getRankedList() {
-		return rankedList;
+
+	public ArrayList<FiveDayForecast> getRankedList() {
+		return weatherList;
 	}
-	
-	public RankForecast(ArrayList<String> weatherList, Activity activity) {
-		this.rankedList = weatherList;
+
+	public RankForecast(ArrayList<FiveDayForecast> weatherList, Activity activity) {
+		this.weatherList = weatherList;
 		this.activity = activity;
 	}
-	
+
+	public ArrayList<FiveDayForecast> rankItems(){
+
+		Integer diffTemperatureMax;
+		Integer diffTemperatureMin;
+
+		// day part
+		int diffPrecipChance;
+		int diffCloudCover;
+		Double diffQpf;
+		Double diffQpfSnow;
+		int diffTemperatureHeatIndex;
+		int diffTemperatureWindChill;
+		String diffWindPhrase;
+		
+		Double dayAverage = 0.0;
+		Double lowAverage = 0.0;
+
+		for(FiveDayForecast day: this.weatherList) {
+			diffTemperatureMax = Math.abs(day.getTemperatureMax() - activity.getBestTemperatureMax());
+			diffTemperatureMin = Math.abs(day.getTemperatureMin() - activity.getBestTemperatureMin());
+
+			// day part
+			diffPrecipChance = Math.abs(day.getPrecipChanceD() - activity.getBestPrecipChance());
+			diffCloudCover = Math.abs(day.getCloudCoverD() - activity.getBestCloudCover());
+			diffQpf = Math.abs(day.getQpfD() - activity.getBestQpf()) * 10;
+			diffQpfSnow = Math.abs(day.getQpfSnowD() - activity.getBestQpfSnow()) * 10;
+			diffTemperatureHeatIndex = Math.abs(day.getTemperatureHeatIndexD() - activity.getBestTemperatureHeatIndex());
+			diffTemperatureWindChill = Math.abs(day.getTemperatureWindChillD() - activity.getBestTemperatureWindChill());
+			//diffWindPhrase = Math.abs(day.getWindPhraseD() - activity.getBestWindPhrase());
+			
+			dayAverage = (diffTemperatureMax + diffTemperatureMin + diffPrecipChance + diffCloudCover + diffQpf + diffQpfSnow + diffTemperatureHeatIndex + diffTemperatureWindChill) / 8;
+			if (dayAverage < lowAverage) {
+				
+			}
+		}
+
+		return rankedList;
+	}
+
 }
